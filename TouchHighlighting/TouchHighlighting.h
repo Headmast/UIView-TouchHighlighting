32f6d17307bff008e8227e2257951456e47df8e0
Kirill <kirill.k@just.ru.com>
2019-11-22 13:09:30 +0300

    Add framework

---
+++ TouchHighlighting/TouchHighlighting.h
@@ -0,0 +1,22 @@
+//
+//  TouchHighlighting.h
+//  TouchHighlighting
+//
+//  Created by Kirill Klebanov on 11/22/19.
+//  Copyright © 2019 Muhammad Tayyab Akram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TouchHighlighting.
+FOUNDATION_EXPORT double TouchHighlightingVersionNumber;
+
+//! Project version string for TouchHighlighting.
+FOUNDATION_EXPORT const unsigned char TouchHighlightingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchHighlighting/PublicHeader.h>
+
+
+#import "MTViewHighlighter.h"
+#import "UIView+TouchHighlighting.h"
+#import "MTCompoundButton.h"
